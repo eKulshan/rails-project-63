8f9a93d5cb5b0847f86c130d9f3b6a5e667bafbc
Evgenii Kulshan <evgenii.k@devstark.com>
2024-01-23 23:40:52 +0300

    add form_for method tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,3 +4,13 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "hexlet_code"
 
 require "minitest/autorun"
+
+User = Struct.new(:name, :job, :gender, keyword_init: true)
+
+def read_fixtures
+  res = {}
+  Dir.each_child(File.expand_path("./fixtures", __dir__)) do |f|
+    res[File.basename(f, ".html")] = File.read(File.expand_path("./fixtures/#{f}", __dir__))
+  end
+  res
+end
--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -1,21 +1,57 @@
 # frozen_string_literal: true
 
 require "test_helper"
-
 class TestHexletCode < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::HexletCode::VERSION
   end
 
   def setup
-    @user = Struct.new(:name, :job)
+    @fixtures = read_fixtures
+    @user = User.new name: "rob", job: "hexlet", gender: "m"
   end
 
   def test_it_generates_form
-    assert_equal '<form action="#" method="post"></form>', HexletCode.form_for(@user)
+    actual = HexletCode.form_for(@user)
+    assert_equal @fixtures["form"], actual
   end
 
   def test_it_generates_form_with_url
-    assert_equal '<form action="/users" method="post"></form>', HexletCode.form_for(@user, url: "/users")
+    actual = HexletCode.form_for(@user, url: "/users")
+    assert_equal @fixtures["form_with_url"], actual
+  end
+
+  def test_it_generates_form_with_input
+    actual = HexletCode.form_for @user do |f|
+      f.input :name
+      f.input :job
+    end
+    assert_equal @fixtures["form_with_input"], actual
+  end
+
+  def test_it_generates_form_with_input_as
+    actual = HexletCode.form_for @user do |f|
+      f.input :name
+      f.input :job, as: :text
+    end
+    assert_equal @fixtures["form_with_input_as"], actual
+  end
+
+  def test_it_generates_form_with_input_additional_attrs
+    actual = HexletCode.form_for @user do |f|
+      f.input :name, class: "user-input"
+      f.input :job
+    end
+    assert_equal @fixtures["form_with_input_additional_attrs"], actual
+  end
+
+  def test_it_raise_on_struct_miising_method
+    assert_raises NoMethodError do
+      HexletCode.form_for @user, url: "/users" do |f|
+        f.input :name
+        f.input :job, as: :text
+        f.input :age # missing field
+      end
+    end
   end
 end
