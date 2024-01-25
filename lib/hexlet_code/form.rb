6b56d7e2ab01b0dbb65d410fd29d29bb33e09f66
Evgenii Kulshan <evgenii.k@devstark.com>
2024-01-25 12:31:32 +0300

    feat: input label

--- lib/hexlet_code/form.rb
+++ lib/hexlet_code/form.rb
@@ -26,14 +26,28 @@ module HexletCode
       @submit = value
     end
 
+    def build_inputs_string(presenter)
+      @inputs.map do |i|
+        input_label = Tag.build(:label, { for: i.dig(:attrs, :name) }, presenter) do
+          i.dig(:attrs, :name).capitalize
+        end.prepend("  ")
+        input = Tag.build_input(i, presenter).prepend("  ")
+        "#{input_label}\n#{input}"
+      end.join("\n")
+    end
+
+    def build_submit_string(presenter)
+      if @submit.nil?
+        ""
+      else
+        Tag.build(:input, { type: "submit", value: @submit },
+                  presenter).prepend("\n  ")
+      end
+    end
+
     def to_s(presenter)
-      inputs_string = @inputs.map { |i| Tag.build_input(i, presenter).prepend("  ") }.join("\n")
-      submit_string = if @submit.nil?
-                        ""
-                      else
-                        Tag.build(:input, { type: "submit", value: @submit },
-                                  presenter).prepend("\n  ")
-                      end
+      inputs_string = build_inputs_string(presenter)
+      submit_string = build_submit_string(presenter)
 
       form_body = "#{inputs_string}#{submit_string}"
       Tag.build(:form, @attrs, presenter) { form_body.empty? ? "" : "\n#{form_body}\n" }
--- lib/hexlet_code/tag.rb
+++ lib/hexlet_code/tag.rb
@@ -7,7 +7,7 @@ module HexletCode
       br: { default_attrs: {} },
       img: { default_attrs: {} },
       div: { default_attrs: {} },
-      label: { type: "single", default_attrs: {} },
+      label: { type: "double", default_attrs: {} },
       input: { type: "single", default_attrs: { type: "text" } },
       textarea: { type: "double", default_attrs: { cols: 20, rows: 40 } },
       form: { type: "double", default_attrs: { action: "#", method: "post" } }
