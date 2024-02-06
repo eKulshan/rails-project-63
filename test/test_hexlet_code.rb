dd4e6c009f3c4883644411bb57729931bf3bbd87
Evgenii Kulshan <evgenii.k@devstark.com>
2024-02-06 11:33:41 +0300

    refactor: read single fixture at time

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -7,47 +7,53 @@ class TestHexletCode < Minitest::Test
   end
 
   def setup
-    @fixtures = read_fixtures
+    # @fixtures = read_fixtures
     @user = User.new name: 'rob', job: 'hexlet', gender: 'm'
   end
 
   def test_it_generates_form
+    expected = read_fixture('form')
     actual = HexletCode.form_for(@user)
-    assert_equal @fixtures['form'], actual
+    assert_equal expected, actual
   end
 
   def test_it_generates_form_with_url
+    expected = read_fixture('form_with_url')
     actual = HexletCode.form_for(@user, url: '/users')
-    assert_equal @fixtures['form_with_url'], actual
+    assert_equal expected, actual
   end
 
   def test_it_generates_form_with_additional_attributes
+    expected = read_fixture('form_with_additional_attributes')
     actual = HexletCode.form_for @user, url: '/profile', method: :get, class: 'hexlet-form', &:submit
-    assert_equal @fixtures['form_with_additional_attributes'], actual
+    assert_equal expected, actual
   end
 
   def test_it_generates_form_with_input
+    expected = read_fixture('form_with_input')
     actual = HexletCode.form_for @user do |f|
       f.input :name
       f.input :job
     end
-    assert_equal @fixtures['form_with_input'], actual
+    assert_equal expected, actual
   end
 
   def test_it_generates_form_with_input_as
+    expected = read_fixture('form_with_input_as')
     actual = HexletCode.form_for @user do |f|
       f.input :name
       f.input :job, as: :text
     end
-    assert_equal @fixtures['form_with_input_as'], actual
+    assert_equal expected, actual
   end
 
   def test_it_generates_form_with_input_additional_attrs
+    expected = read_fixture('form_with_input_additional_attrs')
     actual = HexletCode.form_for @user do |f|
       f.input :name, class: 'user-input'
       f.input :job
     end
-    assert_equal @fixtures['form_with_input_additional_attrs'], actual
+    assert_equal expected, actual
   end
 
   def test_it_raise_on_struct_miising_method
@@ -61,20 +67,22 @@ class TestHexletCode < Minitest::Test
   end
 
   def test_it_generates_form_with_submit
+    expected = read_fixture('form_with_submit')
     actual = HexletCode.form_for @user do |f|
       f.input :name
       f.input :job
       f.submit
     end
-    assert_equal @fixtures['form_with_submit'], actual
+    assert_equal expected, actual
   end
 
   def test_it_generates_form_with_submit_value
+    expected = read_fixture('form_with_submit_value')
     actual = HexletCode.form_for @user do |f|
       f.input :name
       f.input :job
       f.submit 'Wow'
     end
-    assert_equal @fixtures['form_with_submit_value'], actual
+    assert_equal expected, actual
   end
 end
